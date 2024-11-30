13e0b53e85b3d6d93de8c7f82415da69fb800512
Paulo Guilherme <pauloguilherme@usp.br>
2024-11-30 13:20:49 -0300

    Calculate bic

--- src/main.py
+++ src/main.py
@@ -27,38 +27,45 @@ def calculate_likelihood(data, order):
 
     return likelihood
 
-def calculate_likelihoods(data, orders, verbose=False):
+def calculate_likelihoods(data, orders):
     likelihoods = []
 
     for order in orders:
         likelihoods.append(calculate_likelihood(data, order))
-
-    if verbose:
-        print("Order Likelihood")
-        for order in orders[:-1]:
-            print(f"{order}     {likelihoods[order]}")
-
     
     return likelihoods
 
-def calculate_likelihood_ratios(data, orders, verbose=False):
-    likelihoods = calculate_likelihoods(data, orders, verbose)
+def calculate_likelihood_ratios(data, orders):
+    likelihoods = calculate_likelihoods(data, orders)
     likelihood_ratios = [likelihoods[i] / likelihoods[-1] for i in orders[:-1]]
     return likelihood_ratios
 
-def calculate_aic(data, orders, verbose=False):
+def calculate_aic(data, orders):
     k = orders[-1]
-    likelihood_ratios = calculate_likelihood_ratios(data, orders, verbose)
+    likelihood_ratios = calculate_likelihood_ratios(data, orders)
     eta = [-2 * math.log(likelihood_ratio) for likelihood_ratio in likelihood_ratios]
     aic = [eta[i] - 2 * (NUMBER_OF_STATES**k - NUMBER_OF_STATES**i) * (NUMBER_OF_STATES - 1) for i in orders[:-1]]
     best_aic = aic.index(min(aic))
     return (aic, best_aic)
 
-    
-def calculate_metrics(data, orders, verbose=False):
-    aic, best_aic = calculate_aic(data, orders, verbose)
+def calculate_bic(data, orders):
+    k = orders[-1]
+    likelihood_ratios = calculate_likelihood_ratios(data, orders)
+    eta = [-2 * math.log(likelihood_ratio) for likelihood_ratio in likelihood_ratios]
+    bic = [eta[i] - (NUMBER_OF_STATES**k - NUMBER_OF_STATES**i) * (NUMBER_OF_STATES - 1) * len(data) for i in orders[:-1]]
+    best_bic = bic.index(min(bic))
+    return (bic, best_bic)
 
-    print(aic, best_aic)
+
+def calculate_metrics(data, orders, verbose=False):
+    aic, best_aic = calculate_aic(data, orders)
+    bic, best_bic = calculate_bic(data, orders)
+
+    if verbose:
+        print("Order \t AIC \t\t BIC")
+        for order in orders[:-1]:
+            print(f"{order} \t {round(aic[order], 3)} \t {round(bic[order], 3)}")
+
 
 def main():
     max_possible_order = 2
