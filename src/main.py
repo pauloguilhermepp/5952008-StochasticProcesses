6e7e37978df3d4e6c86aa207c15672b07b91aea5
Paulo Guilherme <pauloguilherme@usp.br>
2024-12-01 17:04:38 -0300

    Run test over all csv files

--- src/main.py
+++ src/main.py
@@ -1,9 +1,41 @@
+import os
 from MarkovChainEstimator import MarkovChainEstimator
 
 
-def main():
+def test_labeled_data():
+    orders = [0, 1, 2]
+    path = "data/labeled/order"
     mce = MarkovChainEstimator()
-    mce.run(data_path="data/labeled/order2/ex2.csv", max_possible_order=2, verbose=True)
+
+    for order in orders:
+        csv_dir = f"{path}{order}"
+        experiments = os.listdir(f"{path}{order}")
+
+        for experiment in experiments:
+            print(f"CSV Path: {csv_dir}/{experiment}")
+            mce.run(
+                data_path=f"{csv_dir}/{experiment}", max_possible_order=2, verbose=True
+            )
+            print(100 * "-")
+
+    print(2 * "\n")
+
+
+def test_professor_data():
+    groups = [1, 2, 3]
+    path = "data/professor"
+    mce = MarkovChainEstimator()
+
+    for group in groups:
+        csv_path = f"{path}/group0{group}.csv"
+        print(f"CSV Path: {csv_path}")
+        mce.run(data_path=csv_path, max_possible_order=2, verbose=True)
+        print(100 * "-")
+
+
+def main():
+    test_labeled_data()
+    test_professor_data()
 
 
 if __name__ == "__main__":
